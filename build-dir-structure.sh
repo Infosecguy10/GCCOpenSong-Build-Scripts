5984682feadb118aa02a40513f1b58859331c96c
Infosecguy10 <46640921+Infosecguy10@users.noreply.github.com>
2021-04-21 00:48:59 -0400

    Update and rename build-dir-structure to build-dir-structure.sh
    more changes

---
+++ build-dir-structure.sh
@@ -0,0 +1,15 @@
+#-- Build environment directory structure:
+#-- build-dir-structure
+mkdir /var/opt/scripts
+mkdir /var/opt/bulletin
+mkdir /var/opt/songs
+mkdir /var/opt/sets
+mkdir /var/opt/env
+
+#-- pull the sample sets
+cd /var/opt/sets
+wget -i /var/opt/scripts/pull-sampleset-list.txt
+
+#-- get the Bot repo
+cd /opt
+git clone https://github.com/Infosecguy10/GCCOpenSongBot.git
