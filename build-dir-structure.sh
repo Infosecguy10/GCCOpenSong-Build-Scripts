b6ea1c8146043516356ae9d6ed6c4909fa7e452e
Infosecguy10 <46640921+Infosecguy10@users.noreply.github.com>
2021-04-22 01:02:51 -0400

    Update build-dir-structure.sh
    update build script

--- build-dir-structure.sh
+++ build-dir-structure.sh
@@ -8,18 +8,23 @@ mkdir /var/opt/env
 mkdir /var/opt/html
 mkdir /root/.ssh
 
+#-- copy the required scripts
+cd /var/opt/GCCOpenSong-Build-Scripts
+cp pull-sampleset-list.txt /var/opt/scripts
+cp pull-sshkeys.txt /var/opt/scripts
+
+#-- pull the rclone config
+cp /var/opt/GCCOpenSong-Build-Scripts/rclone.conf /root/.config/rclone/
+
 #-- pull the sample sets
 cd /var/opt/sets
 wget -i /var/opt/scripts/pull-sampleset-list.txt
+
+#-- get the SSH keys
+cd /root/.ssh
+wget -i /var/opt/scripts/pull-sshkeys.txt
 
 #-- get the Bot repo
 cd /opt
 git clone https://github.com/Infosecguy10/GCCOpenSongBot.git
 cp /var/opt/env/.env /opt/GCCOpenSongBot
-
-#-- pull the rclone config
-cp /var/opt/GCCOpenSong-Build-Scripts/rclone.conf /root/.config/rclone/
-
-#-- create the SSH keys
-cd /root/.ssh
-wget -i /var/opt/scripts/pull-sshkeys.txt
